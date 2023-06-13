5f60afd6316b81b0c5808603181c17b58509eb9d
avi-11 <singh.avi1109@gmail.com>
2023-06-13 23:52:06 +0530

    --homeUi Responsive--

--- src/components/headers/Footer.js
+++ src/components/headers/Footer.js
@@ -5,19 +5,19 @@ const Footer = () => {
     <div className="bg-white p-8 m-4 rounded-2xl border border-slate-200">
       <div className="flex justify-around p-4">
         <ul>
-          <li className="font-bold text-black text-xl py-4">Explore</li>
+          <li className="font-bold text-black text-xl py-4 sm:mx-4">Explore</li>
           <li>Community</li>
           <li>Trending Blogs</li>
           <li>Vblog APIs</li>
         </ul>
         <ul>
-          <li className="font-bold text-black text-xl py-4">Explore</li>
+          <li className="font-bold text-black text-xl py-4 sm:mx-4">Explore</li>
           <li>Community</li>
           <li>Trending Blogs</li>
           <li>Vblog APIs</li>
         </ul>
         <ul>
-          <li className="font-bold text-black text-xl py-4">Explore</li>
+          <li className="font-bold text-black text-xl py-4 sm:mx-4">Explore</li>
           <li>Community</li>
           <li>Trending Blogs</li>
           <li>Vblog APIs</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
         "banner-bg": "url('/public/images/bannerBg.jpg')",
       },
     },
+    screens: {
+			xl: { max: "1300px" },
+			// => @media (max-width: 1279px) { ... }
+
+			lg: { max: "1023px" },
+			// => @media (max-width: 1023px) { ... }
+
+			md: { max: "767px" },
+			// => @media (max-width: 767px) { ... }
+
+			sm: { max: "639px" },
+			// => @media (max-width: 639px) { ... }
+		},
   },
+
   plugins: [],
 };
